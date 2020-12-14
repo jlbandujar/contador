6499ae6a561e36ba71e780e6d1f02bba962d6ad9
Jose Luis <jlbandujar@gmail.com>
2020-12-14 17:33:41 +0100

    versión 1.1 con el servlert recién creado

---
+++ src/main/java/servlets/contador.java
@@ -0,0 +1,114 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Jose
+ */
+public class contador extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        try {
+            /* TODO output your page here. You may use following sample code. */
+        int contador = 0;
+        
+        //Revisamos si existe la cookie contadorVisitas
+        Cookie[] cookies = request.getCookies();
+        if(cookies != null){
+            for(Cookie c: cookies){
+                if(c.getName().equals("contadorVisitas")){
+                    contador = Integer.parseInt(c.getValue());
+                }
+            }
+        }
+        
+        //incrementamos el contador el uno
+        contador++;
+        //Agregamos la respuesta al navegador
+        Cookie c = new Cookie("contadorVisitas",Integer.toString(contador));
+        //la cookie se almacenara en el cliente por 1 hora (3600 seg)
+        c.setMaxAge(3600);
+        response.addCookie(c);
+        
+        //Mandamos el mensaje al navegador
+        response.setContentType("text/html;charset=UTF-8");
+        out.print("Contador de visitas de cada cliente:" + contador);
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet contador</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet contador at " + request.getContextPath() + "</h1>");
+            out.println("<h2>Visita nº: "+ contador);
+            out.println("</body>");
+            out.println("</html>");
+        } finally {
+            out.close();
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
